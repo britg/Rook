48af64dc07258435700e9d995de6a40b36ef651f
britg <brit@britg.com>
2014-08-23 08:37:13 -0500

    Test

--- Assets/Rook/Scripts/Camera/CameraController.cs
+++ Assets/Rook/Scripts/Camera/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Application.targetFrameRate = 60;
+
 	}
 	
 	// Update is called once per frame
