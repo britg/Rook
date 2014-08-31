f06d21a8024220120fcd3abf1e2d63036db9e6e6
britg <brit@britg.com>
2014-08-31 10:47:43 -0500

    Initial usage of the new Unity gui system

--- Assets/Rook/Scripts/Player/PlayerRotationController.cs
+++ Assets/Rook/Scripts/Player/PlayerRotationController.cs
@@ -6,25 +6,31 @@ public class PlayerRotationController : GameController {
     public float rotateAngle = 60f;
     public float rotateTime = 0.2f;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+    }
 
+    // Update is called once per frame
+    void Update () {
 
+    }
+
+    public void OnTurnRight () {
+        TurnRight();
+    }
+
+    public void OnTurnLeft () {
+        TurnLeft();
+    }
 
     void TurnRight () {
-        iTween.RotateBy(gameObject, iTween.Hash("y", rotateAngle/360f,
+        iTween.RotateBy(gameObject, iTween.Hash("y", rotateAngle / 360f,
             "time", rotateTime));
         EndTurn();
     }
 
     void TurnLeft () {
-        iTween.RotateBy(gameObject, iTween.Hash("y", -rotateAngle/360f,
+        iTween.RotateBy(gameObject, iTween.Hash("y", -rotateAngle / 360f,
             "time", rotateTime));
         EndTurn();
     }
