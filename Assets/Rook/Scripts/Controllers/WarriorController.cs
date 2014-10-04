dbfa4e1212af6bfac12df1791b50f06d443e0070
britg <brit@britg.com>
2014-10-04 08:52:03 -0500

    Warrior controller

---
+++ Assets/Rook/Scripts/Controllers/WarriorController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarriorController : GameController {
+
+    public override IReceiveAction actionReceiver {
+        get {
+            return player.warrior;
+        }
+    }
+
+}
