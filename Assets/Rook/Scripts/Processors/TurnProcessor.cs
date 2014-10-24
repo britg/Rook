ef33d0ca12f20f7460f16395bff1789f374b1214
britg <brit@britg.com>
2014-10-24 07:28:38 -0500

    Turn refactoring

---
+++ Assets/Rook/Scripts/Processors/TurnProcessor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnProcessor : ActionProcessor {
+
+	public TurnService turnService = new TurnService();
+
+	public override string ActionType {
+		get {
+			return "TurnAction";
+		}
+	}
+
+	public override void Process (GameAction action) {
+
+	}
+
+}
---
+++ Assets/Rook/Scripts/Services/TurnService.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnService {
+
+	public int currentTurn = 1;
+	public bool isPlayerTurn = false;
+
+	public void StartPlayerTurn () {
+		currentTurn++;
+		isPlayerTurn = true;
+	}
+}
